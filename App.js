6b602fcbc884e17fb25db2802ca8acf5d1fe780a
thang.trinh <thang.trinh@unicorn.vn>
2018-01-16 18:20:47 +0700

    apply navigation

--- App.js
+++ App.js
@@ -4,66 +4,63 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
 
 import SplashScreen from "react-native-smart-splash-screen";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev m' +
+            'enu'
 });
 
-export default class App extends Component<{}> {
+export default class App extends Component < {} > {
 
-  componentDidMount () {
-    //SplashScreen.close(SplashScreen.animationType.scale, 850, 500)
-    SplashScreen.close({
-       animationType: SplashScreen.animationType.scale,
-       duration: 850,
-       delay: 500,
-    })
-  }
+    componentDidMount() {
+        //SplashScreen.close(SplashScreen.animationType.scale, 850, 500)
+        SplashScreen.close(
+            {animationType: SplashScreen.animationType.scale, duration: 850, delay: 500}
+        )
+    }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit App.js
+                </Text>
+                <Text style={styles.instructions}>
+                    {instructions}
+                </Text>
+                <Button
+                    onPress={() => this.props.navigation.navigate('Main')}
+                    title="Learn More"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5
+    }
 });
--- RootNavigator.js
+++ RootNavigator.js
@@ -1,11 +1,18 @@
 import App from './App';
 import { StackNavigator } from 'react-navigation';
+import MainScreen from './app/screens/MainScreen';
 
 export default RootNavigator = StackNavigator({
     App: {
         screen: App,
         navigationOptions: {
             title: 'App'
+        }
+    },
+    Main: {
+        screen: MainScreen,
+        navigationOptions: {
+            title: 'Main'
         }
     }
 });

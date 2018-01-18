af6a372c190a298fc86f4f99423966caa291d5d1
Trong Lu <trong.lu@unicorn.vn>
2018-01-18 14:30:23 +0700

    Create Menu screen

---
+++ src/screens/Menu/MenuScreen.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { View, Image, Text, TouchableOpacity, Alert } from 'react-native';
+import styles from './styles';
+
+export default class MenuScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: ['Item 1', 'Item 2', 'Item 3'],
+      profile: {
+        avatarUrl: 'http://www.whatnetworth.com/wp-content/uploads/2017/08/jessica-henwick.jpg',
+        name: 'Jessica Henwick',
+        position: 'Agent',
+      },
+    };
+  }
+
+  onItemPress(value, number) {
+    alert(value);
+  }
+
+  getItemView(value, number) {
+    return (
+      <TouchableOpacity
+        key={number}
+        style={styles.itemContainer}
+        onPress={() => this.onItemPress(value, number)}
+      >
+        <Text>{value}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  render() {
+    return (
+      <View>
+        <View style={styles.profile}>
+          <Image
+            source={{
+              uri: this.state.profile.avatarUrl,
+            }}
+            style={styles.avatar}
+          />
+          <Text style={styles.name}>{this.state.profile.name}</Text>
+          <Text>{this.state.profile.position}</Text>
+        </View>
+        <View>{this.state.data.map(this.getItemView.bind(this))}</View>
+      </View>
+    );
+  }
+}
---
+++ src/screens/Menu/styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  profile: {
+    alignItems: 'center',
+    padding: 16,
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+  },
+  name: {
+    fontSize: 20,
+  },
+  itemContainer: {
+    marginTop: 1,
+    backgroundColor: '#F8F8FF',
+    padding: 8,
+  },
+});
+
+export default styles;

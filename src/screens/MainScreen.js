d2994612929a70d89ed6daf926c20ff3c6654c1c
thangtk <thangtk.dev@hotmail.com>
2018-01-18 10:17:27 +0700

    add missing files

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,33 +1,36 @@
-import React, {Component} from 'react';
-import {View, Text, Image, StyleSheet, Button} from 'react-native';
-import {TabNavigator} from 'react-navigation';
+import React, { Component } from 'react';
+import { View, Text, Image, StyleSheet, Button } from 'react-native';
+import { TabNavigator } from 'react-navigation';
 import HomeScreen from './HomeScreen';
 import DummyScreen from './DummyScreen';
 import tabStyles from './styles';
-import { NavigationActions } from 'react-navigation'
+import { NavigationActions } from 'react-navigation';
 
 const styles = StyleSheet.create({
-    icon: {
-        width: 26,
-        height: 26
-    }
+  icon: {
+    width: 26,
+    height: 26,
+  },
 });
 
-export default MainScreen = TabNavigator({
+export default (MainScreen = TabNavigator(
+  {
     Home: {
-        screen: HomeScreen
+      screen: HomeScreen,
     },
     Dummy: {
-        screen: DummyScreen
+      screen: DummyScreen,
     },
     Dummy2: {
-        screen: DummyScreen
-    }
-}, {
+      screen: DummyScreen,
+    },
+  },
+  {
     tabBarPosition: 'bottom',
     animationEnabled: true,
     tabBarOptions: {
-        activeTintColor: '#e91e63',
-        showIcon: true
-    }
-});
+      activeTintColor: '#e91e63',
+      showIcon: true,
+    },
+  },
+));

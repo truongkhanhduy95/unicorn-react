e41e79eb6a76176e5a87d336664d1137e43fe5f4
thangtk <thangtk.dev@hotmail.com>
2018-01-23 22:05:39 +0700

    Integrate web project

---
+++ app/web/components/HelloWorld.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class HelloWorld extends Component {
+  render() {
+    const { onClick, color } = this.props;
+    return (
+      <div className="hello-world" onClick={onClick} style={{ color }}>
+        Hello World
+      </div>
+    );
+  }
+}
+
+HelloWorld.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  color: PropTypes.string.isRequired,
+};
